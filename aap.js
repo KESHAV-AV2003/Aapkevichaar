2536d576a01fcdb78a3f5d5bb3298b8d6de08de5
KeshavAV <68774478+KESHAV-AV2003@users.noreply.github.com>
2023-02-02 20:31:34 +0530

    Added Files

---
+++ aap.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const path = require("path");
+const port = 80;
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// const document=require("document");
+
+app.engine('html', require('ejs').renderFile);
+app.get('/', function (req, res) {
+    res.render(__dirname + '/views/av2.html');
+});
+//EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')); //For serving static files -->
+app.use(express.urlencoded({ extended: true }));
+
+
+//HTML SPECIFIC STUFF
+app.set('view engine', 'html'); //Set the template engine as pug
+app.set('views', path.join(__dirname, '/views')); //Set the views directory
+
+//END POINTS 
+app.get('/home', (req, res) => {
+    res.status(200).render('av2.html');
+});
+app.get('/about', (req, res) => {
+    res.render('aboutus.html');
+});
+app.get('/inbox', (req, res) => {
+    res.render('inbox.html');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contactav.html');
+});
+
+app.get('/explore', (req, res) => {
+    res.render('exploreav.html');
+});
+app.get('/gmail', (req, res) => {
+    res.render('gmail.html');
+});
+
+
+
+
+  
+
+//START THE SERVER
+app.listen(port, () => {
+    console.log(`The application started successfully on port ${port}`);
+});
+
